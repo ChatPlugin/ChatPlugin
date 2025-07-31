6975d52d0c8e3d9d8b1a226d61149ed63a9296a8
Remigio07 <remigio07@outlook.it>
2025-07-31 15:34:09 +0200

    Added missing license header

--- api/src/main/java/me/remigio07/chatplugin/api/server/util/manager/MSPTManager.java
+++ api/src/main/java/me/remigio07/chatplugin/api/server/util/manager/MSPTManager.java
@@ -1,3 +1,18 @@
+/*
+ * 	ChatPlugin - A complete yet lightweight plugin which handles just too many features!
+ * 	Copyright 2025  Remigio07
+ * 	
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU Affero General Public License for more details.
+ * 	
+ * 	You should have received a copy of the GNU Affero General Public License
+ * 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 	
+ * 	<https://remigio07.me/chatplugin>
+ */
+
 package me.remigio07.chatplugin.api.server.util.manager;
 
 import java.util.Collections;
